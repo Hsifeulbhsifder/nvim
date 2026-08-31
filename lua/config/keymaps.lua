0d5e124f2bef513549e7c68a52d6d5dc4681aed1
Zaeem Mohamed <zaeemkmohamed@gmail.com>
2026-08-31 16:53:06 -0700

    xxd support

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,3 +13,36 @@ keymap("n", "<leader>rN", ":PrevError<CR>", opts)
 keymap("n", "<leader>ut", function()
   vim.opt.scrolloff = 999 - vim.o.scrolloff
 end, { desc = "Toggle scroll lock" })
+
+local function toggle_hex()
+  if vim.b.hex_mode then
+    vim.api.nvim_clear_autocmds({ event = { "BufWritePre", "BufWritePost" }, buffer = 0 })
+    vim.cmd("silent! %!xxd -r")
+    vim.opt_local.binary = false
+    vim.bo.filetype = ""
+    vim.bo.modified = false
+    vim.b.hex_mode = false
+  else
+    vim.opt_local.binary = true
+    vim.cmd("silent! %!xxd")
+    vim.bo.filetype = "xxd"
+    vim.b.hex_mode = true
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = 0,
+      callback = function()
+        vim.cmd("silent! %!xxd -r")
+      end,
+    })
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      buffer = 0,
+      callback = function()
+        vim.cmd("silent! %!xxd")
+        vim.bo.modified = false
+      end,
+    })
+  end
+end
+
+keymap("n", "<leader>hx", toggle_hex, { desc = "Toggle hex view (xxd)" })
+>>>>>>> dce9518 (xxd support)
